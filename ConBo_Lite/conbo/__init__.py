6c27a1534f2790c874d3fe9e0c9e1820f08d9f02
qinjxbr <126067381+qinjxbr@users.noreply.github.com>
2026-07-22 18:19:18 +0800

    Delete ConBo_Lite directory

--- ConBo_Lite/conbo/__init__.py
+++
@@ -1,7 +0,0 @@
-"""ConBo Lite: shared tracking and ROI motion-magnification pipeline."""
-
-from .config import ConBoConfig
-from .pipeline import ConBoPipeline
-from .adaptive_mvgc import AdaptiveMVGCResult, tune_adaptive_mvgc
-
-__all__ = ["ConBoConfig", "ConBoPipeline", "AdaptiveMVGCResult", "tune_adaptive_mvgc"]
